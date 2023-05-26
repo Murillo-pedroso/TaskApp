02a612974f31ad8ee25b585f19b81973be61509d
murillo.pedroso <murillo.pedroso@jiveinvestments.com>
2023-05-26 16:32:49 -0300

    tdd implement(app): implementado o tdd do projeto escrito em jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,6 +8,28 @@ test("Adicionar Tarefa", () => {
   const input = screen.getByPlaceholderText(/Nova Tarefa/i);
   fireEvent.change(input, { target: { value: "Levar o lixo" } });
   const addBtn = screen.getByText(/Adicionar Tarefa/i);
-  addBtn.click();
-  expect("Levar o lixo").toContain("Levar o lixo");
+  fireEvent.click(addBtn);
+  expect(screen.getByText("Levar o lixo")).toBeInTheDocument();
+});
+
+test("Finalizar Tarefa", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText(/Nova Tarefa/i);
+  fireEvent.change(input, { target: { value: "Comprar pão" } });
+  const addBtn = screen.getByText(/Adicionar Tarefa/i);
+  fireEvent.click(addBtn);
+  const finalizeBtn = screen.getByText(/Finalizar Tarefa/i);
+  fireEvent.click(finalizeBtn);
+  expect(screen.getByText(/Finalizada/i)).toBeInTheDocument();
+});
+
+test("Deletar Tarefa", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText(/Nova Tarefa/i);
+  fireEvent.change(input, { target: { value: "Fazer jantar" } });
+  const addBtn = screen.getByText(/Adicionar Tarefa/i);
+  fireEvent.click(addBtn);
+  const deleteBtn = screen.getByText(/Deletar/i);
+  fireEvent.click(deleteBtn);
+  expect(screen.queryByText("Fazer jantar")).toBeNull();
 });
