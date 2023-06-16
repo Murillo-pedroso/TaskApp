8318676c96da324a3276753d6eb0505eb94c9a7f
e_sabrinafs <saahteresaah@gmail.com>
2023-06-16 16:56:40 -0300

    Teste - Marcar tarefa como não finalizada

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -24,3 +24,34 @@ test("deleta uma tarefa", () => {
   // Verifica se a tarefa foi removida da lista
   expect(taskItem).not.toBeInTheDocument();
 });
+
+test("marca uma tarefa como não finalizada", () => {
+  render(<App />);
+
+  // Adiciona uma tarefa
+  const addTaskButton = screen.getByText("Adicionar Tarefa");
+  const inputElement = screen.getByPlaceholderText(
+    "Nova Tarefa"
+  ) as HTMLInputElement;
+  fireEvent.change(inputElement, { target: { value: "Nova Tarefa" } });
+  fireEvent.click(addTaskButton);
+
+  // Verifica se a tarefa foi adicionada à lista
+  const taskItem = screen.getByText("Nova Tarefa");
+  expect(taskItem).toBeInTheDocument();
+
+  // Marca a tarefa como finalizada
+  const finalizeButton = screen.getByTestId(/finalize-task-.*/);
+  fireEvent.click(finalizeButton);
+
+  // Verifica se a tarefa foi marcada como finalizada
+  const finalizedTaskItem = screen.getByText("Finalizada");
+  expect(finalizedTaskItem).toBeInTheDocument();
+
+  // Marca a tarefa como não finalizada
+  fireEvent.click(finalizeButton);
+
+  // Verifica se a tarefa foi marcada como não finalizada
+  const notFinalizedTaskItem = screen.getByText("Tarefa em Andamento");
+  expect(notFinalizedTaskItem).toBeInTheDocument();
+});
